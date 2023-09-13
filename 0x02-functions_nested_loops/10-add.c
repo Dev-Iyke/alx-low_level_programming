ebc0248fa77d88197fabc37a415a88d11b13407c
Dev-Iyke <iyke.thompson01@gmail.com>
2023-09-12 17:36:44 -0700

    a plus b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function to add two parameters
+ * @a: parameter one
+ * @b: parameter two
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
