997a2d53b580684e239de1f2f93f5a7ea01bc820
Dev-Iyke <iyke.thompson01@gmail.com>
2023-09-18 10:59:27 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - function to print a string to stdout
+ * @str: function parameter
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int s;
+
+	for (s = 0 ; str[s] != '\0' ; s++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
