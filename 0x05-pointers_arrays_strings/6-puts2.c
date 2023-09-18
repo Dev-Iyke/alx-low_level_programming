bbd9c2e69ea9fb307661041ce1ae2c9aced98cb3
Dev-Iyke <iyke.thompson01@gmail.com>
2023-09-18 12:04:58 -0700

    Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print even numbers from a string
+ * @str: function parameter
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
