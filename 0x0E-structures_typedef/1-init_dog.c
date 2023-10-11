d6170f5a3ab2dbfac70972789691fa2a06cff06f
Dev-Iyke <iyke.thompson01@gmail.com>
2023-10-10 22:25:09 -0700

    Poppy

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * init_dog - function to initialize a variable type
+ * @d: struct pointer
+ * @name: dog name
+ * @age: dog age
+ * @owner: dogs owner
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: function parameter
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+
+}
