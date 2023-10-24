d9a0b1546c0f632ae20261154943208d1ebabcce
Obasi Ikechukwu Thompson <obasiikechukwu761@gmail.com>
2023-10-24 19:45:48 +0100

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,5 +9,30 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	
+	listint_t *last_n;
+	listint_t *tmp;
+
+	(void)tmp;
+
+	last_n = malloc(sizeof(listint_t));
+
+	if (last_n == NULL)
+		return (NULL);
+	last_n->n = n;
+	last_n->next = NULL;
+	tmp = *head;
+	if (*head == NULL)
+	{
+		*head = last_n;
+	}
+	else
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = last_n;
+	}
+
+	return (*head);
 }
