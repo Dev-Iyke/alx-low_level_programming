71700968c6a0b783d00a49fb05b44ad526099510
Obasi Ikechukwu Thompson <obasiikechukwu761@gmail.com>
2023-10-26 10:38:20 +0100

    basic files and task 0

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_binary - function that converts and prints the binary equivalent of an integer
+ * @n: integer to convert
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+
+}
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_bit - function returns the value of a bit at a given index
+ * @index: index of number
+ * Return: an interger
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+}
