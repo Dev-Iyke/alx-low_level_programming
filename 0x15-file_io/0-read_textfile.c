9eabc480797def3b53c444f1a0593f24a110664a
Dev-Iyke <iyke.thompson01@gmail.com>
2023-10-30 16:33:57 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,13 +19,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
+
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
-	nread = read(fd, buff, letters);
 
+	nread = read(fd, buff, letters);
 	nwrite = write(STDOUT_FILENO, buff, nread);
 
 	close(fd);
