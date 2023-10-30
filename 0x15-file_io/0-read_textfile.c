2501b27ebc5841de3230ff7c462a90e2d3f5fb2e
Obasi Ikechukwu Thompson <obasiikechukwu761@gmail.com>
2023-10-31 00:21:09 +0100

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output.
+ *
+ * @filename: the file to read
+ * @letters: number of letters to be printed
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nread, nwrite;
+	char *buff;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename,O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	nread = read(fd, buff, letters);
+
+	nwrite = write(STDOUT_FILENO, buff, nread);
+
+	close(fd);
+	free(buff);
+
+	return (nwrite);
+}
